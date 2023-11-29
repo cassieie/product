d0135ddadcce3b14cfc5e6a74c3e38995a6069a7
cassieie <f21h1234@gmail.com>
2023-11-30 00:13:04 +0800

    product3

--- product.py
+++ product.py
@@ -1,11 +1,16 @@
 product = []
-with open('product.cvs', 'r', encoding='utf-8') as f:
-	for s in f:
-		if '商品,價格' in s:
-			continue
-		name, price = s.strip().split(',')
-		product.append([name, price])
-print(product)
+import os
+if os.path.isfile('product.cvs'):
+	print('yes')
+	with open('product.cvs', 'r', encoding='utf-8') as f:
+		for s in f:
+			if '商品,價格' in s:
+				continue
+			name, price = s.strip().split(',')
+			product.append([name, price])
+	print(product)
+else:
+	print('no')
 
 while True:
 	name = input('名稱')
